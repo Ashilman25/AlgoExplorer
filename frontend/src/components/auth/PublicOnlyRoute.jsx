210fc313238a026c0f58b5fbc50bc2772e045174
Andrew Shilman <Andrew.Shilman@gmail.com>
2026-03-25 20:29:40 -0400

    Generic route for all users

---
+++ frontend/src/components/auth/PublicOnlyRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate } from 'react-router-dom'
+import Spinner from '../ui/Spinner'
+import { useAuthStore } from '../../stores/useAuthStore'
+
+
+export default function PublicOnlyRoute({ children }) {
+  const accessToken = useAuthStore((s) => s.accessToken)
+  const user = useAuthStore((s) => s.user)
+  const isInitialized = useAuthStore((s) => s.isInitialized)
+
+  if (!isInitialized) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-950">
+        <Spinner size="lg" />
+      </div>
+    )
+  }
+
+  if (accessToken && user) {
+    return <Navigate to="/account" replace />
+  }
+
+  return children
+}
